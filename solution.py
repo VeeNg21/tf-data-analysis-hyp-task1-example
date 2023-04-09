5cea09c1840af47c727ca7a4a7a41996dd1b088f
Vee21 <55953389+VeeNg21@users.noreply.github.com>
2023-04-09 23:28:53 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
+def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
+  p1 = x_success / x_cnt
+  p2 = y_success / y_cnt
+  p_pool = (x_success + y_success) / (x_cnt + y_cnt)
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+  z = (p2 - p1) / np.sqrt(p_pool * (1 - p_pool) * (1/x_cnt + 1/y_cnt))
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+# Find the corresponding p-value and compare to alpha level
+  p_value = 1 - norm.cdf(z)
+  alpha = 0.02
+  reject_null = p_value < alpha
+
+return reject_null
